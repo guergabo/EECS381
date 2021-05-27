931d20755fde6552082fe2a8144817cb20c1e75f
guergabo <guergabo@umich.edu>
2021-05-26 22:57:02 -0400

    start

---
+++ P2/Project02/p2_main.cpp
@@ -0,0 +1,12 @@
+// CPP program for project 2, Micro Meeting Manager, EECS 381
+
+// interfaces
+
+// libraries
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Project 2 " << '\n';
+    return 0;
+}
